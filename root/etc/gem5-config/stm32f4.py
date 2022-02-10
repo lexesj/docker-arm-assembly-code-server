e40b1127ee035c88349510f5fb0873a10dfde0ce
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2022-02-10 22:27:28 +0000

    Remove unused cli flags and classes

--- root/etc/gem5-config/stm32f4.py
+++ root/etc/gem5-config/stm32f4.py
@@ -6,18 +6,13 @@ import argparse
 import m5
 # import all of the SimObjects
 from m5.objects import *
-#from m5.util import *
+# from m5.util import *
 
-from common import parse_range, CM4XBar, CM4Minor
+from common import CM4XBar, CM4Minor
 
 parser = argparse.ArgumentParser()
 parser.add_argument('rom', help='ROM to load')
 parser.add_argument('--wait-gdb', action='store_true', help='Wait for GDB')
-parser.add_argument('--test-data', help='Test data file')
-parser.add_argument('--test-addr', default=0x20004000, help='Test data load address')
-parser.add_argument('--test-pc', default=0, help='Load test data when PC reaches specific value')
-parser.add_argument('--dump-ranges', help='Dump range(s) of memory after simulation (start address:size[,...])',
-    type=lambda a: [parse_range(r) for r in a.split(',')])
 
 args = parser.parse_args()
 
@@ -70,27 +65,16 @@ system.system_port = system.membus.cpu_side_ports
 system.workload = ARMROMWorkload(rom_file=args.rom)
 
 # Set the cpu to use the process as its workload and create thread contexts
-system.cpu.tracer = PerfgradeTracer()
-system.cpu.wait_for_remote_gdb = args.wait_gdb
+if args.wait_gdb:
+    system.workload.wait_for_remote_gdb = True
 system.cpu.createThreads()
 
 # set up the root SimObject and start the simulation
 root = Root(full_system=True, system=system)
-root.dumper = MemDump(system=system)
 
 # instantiate all of the objects we've created above
 m5.instantiate()
 
-if args.test_data:
-    if args.test_pc:
-        root.dumper.loadWhen(args.test_data, Addr(args.test_addr), Addr(args.test_pc))
-    else:
-        root.dumper.load(args.test_data, Addr(args.test_addr))
-
 print("Beginning simulation!")
 exit_event = m5.simulate()
 print('Exiting @ tick %i because %s' % (m5.curTick(), exit_event.getCause()))
-
-if args.dump_ranges:
-    for i, r in enumerate(args.dump_ranges):
-        root.dumper.dump(f'mem{i}.bin', r.getValue())
